3be511a1de75d73ca8c97208b2c1cb950116215e
youngmin <wogitopp@naver.com>
2022-06-13 10:11:58 +0900

    프런트 동작 js파일 생성

--- app/app.js
+++ app/app.js
@@ -9,7 +9,9 @@ const home = require("./src/routes/home/index"); // 라우팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// 미들웨어
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`)); // 정적경로 추가
 
 
 module.exports = app;
