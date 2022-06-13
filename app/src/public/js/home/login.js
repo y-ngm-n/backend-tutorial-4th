d46c17645da058e692e143d782d4466bf12d2921
youngmin <wogitopp@naver.com>
2022-06-13 16:42:49 +0900

    로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,5 +19,7 @@ function login() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(req)
-    });
+    })
+    .then((res) => {return res.json();})
+    .then(console.log);
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,12 @@
 "use strict"
 
+// 임시 데이터
+const users = {
+    id: ["y__ngm_n", "song", "young"],
+    pw: ["123", "123", "1234"]
+};
+
+
 // 렌더링
 const view = {
     root: (req, res) => { res.render("home/root"); },
@@ -9,7 +16,20 @@ const view = {
 // 로그인 처리
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id;
+        const pw = req.body.pw;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx]===pw) {return res.json({
+                success: true,
+            });}
+        }
+
+        return res.json({
+            success: false,
+            msg: "login failed",
+        });
     }
 };
 
