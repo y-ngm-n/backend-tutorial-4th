36129096fc12e5ce12a0418d10fb1eaf438fb9a5
youngmin <wogitopp@naver.com>
2022-06-15 23:21:51 +0900

    백엔드 맛보기 완료

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,15 +6,17 @@ const logger = require("../../config/logger");
 // 렌더링
 const view = {
     root: (req, res) => {
-        logger.info(`GET / 200 "홈 화면으로 이동`);
+        logger.info('GET / 200 "홈 화면으로 이동"');
         res.render("home/root");
     },
+
     login: (req, res) => {
-        logger.info(`GET /login 200 "로그인 화면으로 이동`);
+        logger.info('GET /login 200 "로그인 화면으로 이동"');
         res.render("home/login");
     },
+
     register: (req, res) => {
-        logger.info(`GET /register 200 "회원가입 화면으로 이동`);
+        logger.info('GET /register 200 "회원가입 화면으로 이동"');
         res.render("home/register");
     },
 };
@@ -24,26 +26,39 @@ const process = {
     login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if (response.err) logger.error(
-            `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
-        );
-        else logger.info(
-            `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-        );
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/login",
+            status: response.err ? 400:200,
+        };
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if (response.err) logger.error(
-            `POST /register 200 Response: "success: ${response.success}, ${response.err}"`
-        );
-        else logger.info(
-            `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-        );
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/register",
+            status: response.err ? 409:201,
+        };
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 };
 
 module.exports = { view, process };
+
+// 로깅 함수
+const log = (response, url) => {
+    if (response.err) {
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`
+        );
+    } else {
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg || ""}`
+        );
+    }
+}
